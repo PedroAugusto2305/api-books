2ec865239079afd106361056821f79b981529d88
PedroAugusto2305 <p.augusto_23@outlook.com>
2023-10-11 10:11:07 -0300

    Add Routes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import http from "http";
 
-const PORT = 3000;
+const PORT = 3001;
+
+
+const rotas = {
+  "/": "Curso de Express API",
+  "/livros": "Entrei na rota livros",
+  "/autores": "Entrei na rota Autores"
+};
+
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Curso de Node.js");
+  res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () => {
